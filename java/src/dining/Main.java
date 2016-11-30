45318708db789097195f07309a08169dc8423056
stephenr93 <redraider432@live.com>
2016-11-29 21:10:45 -0600

    minor changes

--- java/src/dining/Main.java
+++ java/src/dining/Main.java
@@ -4,19 +4,16 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
  
 /*This Java implementation uses a token system. 
- *If a philosopher's number is on the token, they pick up their left and right forks.
+ *If a philosopher's number is on the token, they pick up their left and right chopsticks.
  *Passing the token to their immediate neighbor would be pointless, 
  *so they increment the token by 2, passing it to the philosopher after their neighbor. 
- *The +2 works well for odd numbers of philosophers.
- * With wait down at 1 millisecond I get about 1.5M eats/sec running 5 philosophers, 
- * down to about 0.5M eats/sec running 25. The single token generates good availability for 80% of 5 forks, 
- * but a much lower availability % of 25 forks. 
+ *The +2 works well for odd numbers of philosophers. 
  */
 enum PhilosopherState { Grab , Eats , Thnk }
 
-public class test {
+public class main {
     static final int philosopherCount = 5; //  5 philosophers
-    static final int runSeconds = 10; //runtime 
+    static final int runSeconds = 10; //runtime in seconds
     //ArrayList used to hold chopstick availability 
     static ArrayList<Chopstick> chopsticks = new ArrayList<Chopstick>();
     //ArrayList used to hold Philosopher order
@@ -44,7 +41,7 @@ public class test {
             for (Chopstick c : chopsticks) {
                 int holder = c.holder.get();
                 //shows who's holding the chopsticks
-                div.append(holder==-1?"   ":String.format("P%02d",holder));
+                div.append(holder==-1?"   ":String.format("P%02d",holder+1));
                 div.append("|");
             }
  
